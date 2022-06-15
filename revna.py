dd104e95e4fbce9a1a0154f1f26e580b7a5a51a0
Gunnar Pope <gunnarpope@gmail.com>
2022-06-14 21:50:26 -0400

    refactored input args; initial functionality is there

--- revna.py
+++ revna.py
@@ -2,7 +2,7 @@
     @brief a personal assistant to improve your daily shredding.
     @author gunnarpope
     @date   6/14/22
-    
+
 """
 from datetime import datetime as dt
 import pandas as pd
@@ -11,7 +11,7 @@ import json
 import os
 
 class Todo():
-    def __init__(self, task, project=None, status='undone',  due=None, level='low', category=None):
+    def __init__(self, task, project=None, status='todo',  due=None, level='low', category=None):
         self.category = category 
         self.task     = task
         self.level    = level 
@@ -19,35 +19,42 @@ class Todo():
         self.origin   = dt.isoformat(dt.now())
         self.due      = due
 
+def GetPending():
+    print(df[ df['status'] != 'done'])
 
 REVNA_ROOT = os.getenv('REVNA_ROOT')
 filename = REVNA_ROOT + 'todo/list.json'
-
-with open(filename, 'rb') as f:
-    tasks = dict(json.load(f))
 
 df_filename = REVNA_ROOT + 'todo/todo.json'
 df = pd.read_json(df_filename)
 df.head()
 
 parser = argparse.ArgumentParser(description="I'm Revna, your personal assistant for getting shit done.")
-parser.add_argument("-t", type=str, help="Add an item to the todo list")
-parser.add_argument("-d", type=str, help="Add a due date to an item")
+parser.add_argument("-t", "--todo",     type=str, help="Add an item to the todo list")
+parser.add_argument("-d", "--due",      type=str, help="Add a due date to an item")
+parser.add_argument("-s", "--status",   type=str, help="The status of the task ( todo | doing | done )")
+parser.add_argument("-i", "--importance",    type=str, help="The importance level of the task ( low | med | high )")
+parser.add_argument("-c", "--category", type=str, help="The category of the task.")
+parser.add_argument("-l", "--list",     type=str, help="List all pending tasks.")
+parser.add_argument("-g", "--go",       type=str, help="Revna, let's go!")
+
 args = parser.parse_args()
 
-print(args)
-print(tasks)
-print('All tasks: ', tasks['todo'])
+if args.todo:
+    print('Todo: ', args.todo)
 
-if args.t:
-    print('Todo: ', args.t)
-    task = Todo(task=args.t)
+    task = Todo(task=args.todo, level=args.importance, status=args.status, due=args.due)
+
     df_new = pd.DataFrame(task.__dict__, index=[1])
     df = pd.concat([df, df_new], ignore_index=True)
     df.to_json(df_filename )
-if args.d:
-    print('Due date: ', args.due)
 
+elif args.list:
+    df[ df['status'] != 'done'].head()
 
-print(df.head())
+elif args.go:
+    print("Hello, Gunnar.")
+    print("Here are your tasks for today:")
+    GetPending()
+
 
